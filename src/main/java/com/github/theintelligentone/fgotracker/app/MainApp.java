87aa6ae037c7b0f78d6cfeff6f39f4ca0d90d835
The Intelligent One <totally.real@email.com>
2021-05-05 22:28:23 +0200

    Start of FXML file for GUI

--- src/main/java/com/github/theintelligentone/fgotracker/app/MainApp.java
+++ src/main/java/com/github/theintelligentone/fgotracker/app/MainApp.java
@@ -1,13 +1,18 @@
 package com.github.theintelligentone.fgotracker.app;
 
-import com.github.theintelligentone.fgotracker.domain.ServantOfUser;
+import com.github.theintelligentone.fgotracker.domain.servant.ServantOfUser;
 import com.github.theintelligentone.fgotracker.service.DataManagementService;
 import javafx.application.Application;
-import javafx.scene.Group;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
+import java.net.URL;
+
 public class MainApp extends Application {
+    private static final String MAIN_WINDOW_FXML = "/mainWindow.fxml";
     private DataManagementService dataManagementService;
 
     public static void main(String[] args) {
@@ -17,11 +22,10 @@ public class MainApp extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         dataManagementService = new DataManagementService();
-        Group group = new Group();
-        Scene scene = new Scene(group);
-        ServantOfUser tempSvt = new ServantOfUser();
-        tempSvt.setLevel(20);
-        dataManagementService.saveUserServant(tempSvt, 0);
+        URL fxmlSource = getClass().getResource(MAIN_WINDOW_FXML);
+        Scene scene = new Scene(FXMLLoader.load(fxmlSource));
+        TableColumn idColumn = new TableColumn("Id");
+        idColumn.setCellValueFactory(new PropertyValueFactory<ServantOfUser, Integer>("id"));
         primaryStage.setScene(scene);
         primaryStage.setTitle("FGO Tracker");
         primaryStage.show();

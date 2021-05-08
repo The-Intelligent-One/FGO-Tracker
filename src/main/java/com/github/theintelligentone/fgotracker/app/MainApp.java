bbefc93dd62c8b6fb19f50952dc4cd6117a332fb
The Intelligent One <totally.real@email.com>
2021-05-08 16:53:29 +0200

    Last of refactor

--- src/main/java/com/github/theintelligentone/fgotracker/app/MainApp.java
+++ src/main/java/com/github/theintelligentone/fgotracker/app/MainApp.java
@@ -1,15 +1,18 @@
 package com.github.theintelligentone.fgotracker.app;
 
 import com.github.theintelligentone.fgotracker.service.DataManagementService;
+import com.github.theintelligentone.fgotracker.ui.MainWindow;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import org.controlsfx.control.textfield.TextFields;
 
 public class MainApp extends Application {
     private static final String MAIN_WINDOW_FXML = "/mainWindow.fxml";
-    private DataManagementService dataManagementService;
+    private FXMLLoader loader;
 
     public static void main(String[] args) {
         launch();
@@ -17,18 +20,18 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource(MAIN_WINDOW_FXML));
+        loader = new FXMLLoader(getClass().getResource(MAIN_WINDOW_FXML));
         Parent root = loader.load();
         Scene scene = new Scene(root);
         scene.getStylesheets().add("tableStyle.css");
         primaryStage.setScene(scene);
         primaryStage.setTitle("FGO Tracker");
-        dataManagementService = new DataManagementService(loader.getController());
         primaryStage.show();
     }
 
     @Override
     public void stop() {
-        dataManagementService.tearDown();
+        MainWindow controller = loader.getController();
+        controller.tearDown();
     }
 }

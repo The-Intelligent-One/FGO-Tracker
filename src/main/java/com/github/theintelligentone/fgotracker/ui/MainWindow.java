fbe0f693e821d9021f44af6131762464b1e0d6fb
The Intelligent One <totally.real@email.com>
2021-05-08 20:16:45 +0200

    Added border and button to add rows

--- src/main/java/com/github/theintelligentone/fgotracker/ui/MainWindow.java
+++ src/main/java/com/github/theintelligentone/fgotracker/ui/MainWindow.java
@@ -4,8 +4,10 @@ import com.github.theintelligentone.fgotracker.domain.servant.ServantOfUser;
 import com.github.theintelligentone.fgotracker.domain.servant.UserServantFactory;
 import com.github.theintelligentone.fgotracker.service.DataManagementService;
 import javafx.collections.ObservableList;
+import javafx.css.PseudoClass;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
 
 public class MainWindow {
@@ -20,10 +22,23 @@ public class MainWindow {
     public void initialize() {
         dataManagementService = new DataManagementService();
         rosterTable.setItems(getUserServants());
-        dataManagementService.saveUserServant(dataManagementService.tempLoad(), 3);
         nameColumn.setOnEditCommit(event -> {
             getUserServants().set(event.getTablePosition().getRow(), new UserServantFactory().replaceBaseServant(event.getRowValue(), dataManagementService.getServantByName(event.getNewValue())));
         });
+
+        PseudoClass lastRow = PseudoClass.getPseudoClass("last-row");
+        rosterTable.setRowFactory(tv -> new TableRow<>() {
+            @Override
+            public void updateIndex(int index) {
+                super.updateIndex(index);
+                pseudoClassStateChanged(lastRow,
+                        index >= 0 && index == rosterTable.getItems().size() - 1);
+            }
+        });
+    }
+
+    public void addNewRow() {
+        dataManagementService.saveUserServant(null);
     }
 
     public void tearDown() {

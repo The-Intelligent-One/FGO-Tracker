7406a1ea8c62907887ff399f2f12b069de12eb56
The Intelligent One <unit88.hun@gmail.com>
2021-12-15 21:08:17 +0100

    ControlsFX temporarily added in for potential use

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,7 @@
 module fgotracker {
+    requires java.desktop;
+    requires javafx.swing;
+    requires javafx.web;
     requires javafx.controls;
     requires javafx.fxml;
     requires spring.boot.autoconfigure;
@@ -12,11 +15,9 @@ module fgotracker {
     requires com.fasterxml.jackson.annotation;
     requires com.fasterxml.jackson.databind;
     requires com.fasterxml.jackson.datatype.jsr310;
-    requires java.desktop;
-    requires javafx.swing;
-    requires javafx.web;
     requires github.api;
     requires com.opencsv;
+    requires org.controlsfx.controls;
 
     opens com.github.theintelligentone.fgotracker.ui.controller to javafx.fxml;
     exports com.github.theintelligentone.fgotracker;

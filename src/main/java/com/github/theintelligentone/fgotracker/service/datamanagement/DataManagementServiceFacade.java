e6074c2af7da371e58884d7a92ae6fee9f910378
The Intelligent One <unit88.hun@gmail.com>
2021-12-28 22:30:53 +0100

    #101 Updated versions final commit for memory fix hopefully

--- src/main/java/com/github/theintelligentone/fgotracker/service/datamanagement/DataManagementServiceFacade.java
+++ src/main/java/com/github/theintelligentone/fgotracker/service/datamanagement/DataManagementServiceFacade.java
@@ -19,7 +19,7 @@ import java.util.List;
 
 @Component
 public class DataManagementServiceFacade {
-    public static final String VERSION = "v0.3.2-hotfix";
+    public static final String VERSION = "v0.3.3-beta";
     public static final int[] MAX_LEVELS = {65, 60, 65, 70, 80, 90};
 
     public static final int MIN_TABLE_SIZE = 25;
